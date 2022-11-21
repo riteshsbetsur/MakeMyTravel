fd5d13b47c8642116d382f29c574cd95e3bd68d4
riteshsbetsur <riteshsbetur@gmail.com>
2022-11-21 16:02:26 +0530

    day1

---
+++ src/components/navbar/Menu.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Styles from "./_navbar.module.css";
+
+const Menu = () => {
+  return (
+    <div className={Styles.menuBlock}>
+      <ul>
+        <li>
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" activeClassName="active">
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/register" activeClassName="active">
+            Register
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
